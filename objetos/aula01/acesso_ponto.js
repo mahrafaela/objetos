eb9f7f6848843ca3e35cdd8c6ba7b9108aea12d4
mahrafaela <128821670+mahrafaela@users.noreply.github.com>
2024-06-03 09:04:45 -0300

    Add files via upload

---
+++ objetos/aula01/acesso_ponto.js
@@ -0,0 +1,9 @@
+const objetoPessoa = {
+    nome: "Rafaela",
+    idade: "16",
+    CPF: "99775534261",
+    email: "maria.ghergoletto@escola.pr.gov.br", 
+
+}
+
+console.log(objetoPessoa)
---
+++ objetos/aula01/objetos.js
@@ -0,0 +1,9 @@
+const informacoesPessoa = ["nome", "Rafaela", "idade", 16, "CPF", "99775534261"];
+
+const objetoPessoa = {
+    nome: "Rafaela",
+    idade: "16",
+    CPF: "99775534261",
+    email: "maria.ghergoletto@escola.pr.gov.br", 
+
+}
