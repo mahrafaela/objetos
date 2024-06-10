a924e6812a76ce99ae77d98ba7bf7da215e395bc
mahrafaela <128821670+mahrafaela@users.noreply.github.com>
2024-06-10 09:07:13 -0300

    Add files via upload

---
+++ Aula02/tipos.js
@@ -0,0 +1,7 @@
+     const cliente = {
+         nome: "Rafaela",
+         idade: 16,
+         email: "maria.ghergoletto@gmail.com",
+         telefone: ["44332909200", "86004321265"]
+ };
+ console.log(cliente)
